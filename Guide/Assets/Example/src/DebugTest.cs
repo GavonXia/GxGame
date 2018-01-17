f7f760ddd46021b1ee2b691c1e897458f47573e2
Gavon.X <578417235@qq.com>
2018-01-17 14:07:55 +0800

    Debug

---
+++ Guide/Assets/Example/src/DebugTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTest : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Log");
+        Debug.LogWarning("LogWarning");
+        Debug.LogError("LogError");
+
+        Debug.Log("Log", gameObject);
+        Debug.LogFormat(gameObject, "GameObject Name : {0}", gameObject.name);
+    }
+}
