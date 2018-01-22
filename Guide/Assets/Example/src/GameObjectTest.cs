7de39327296e9045a0e38c8ce15190650a6a940d
Gavon.X <578417235@qq.com>
2018-01-22 19:50:13 +0800

    GameObjectTest

--- Guide/Assets/Example/src/GameObjectTest.cs
+++ Guide/Assets/Example/src/GameObjectTest.cs
@@ -2,15 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameObjectTest : MonoBehaviour {
+public class GameObjectTest : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] m_tagObjs;
+    [SerializeField]
+    Camera m_camera;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        gameObject.layer = LayerMask.NameToLayer("UI");
+        gameObject.tag = "Player";
+        m_camera = FindObjectOfType<Camera>();
+        m_tagObjs = GameObject.FindGameObjectsWithTag("Player");
+
+
+        GameObject cubeTemplate = GameObject.Find("Cube");
+        GameObject cubeParentNew = Instantiate(cubeTemplate);
+
+        cubeParentNew.name = "new cube parent";
+        cubeParentNew.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+
+        GameObject cubeChildNew = Instantiate(cubeTemplate, Vector3.zero, Quaternion.identity);
+        cubeChildNew.name = "new cube child";
+        cubeChildNew.transform.SetParent(cubeParentNew.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
