6300732d27993e669b43962e94f348f1bb4cff73
Gavon.X <578417235@qq.com>
2018-01-31 19:14:29 +0800

    aa

---
+++ UIToolKit/Assets/Standard Assets/Demigiant/DoTweenExt/GalaxyTweenController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DG.Tweening.GaExtention
+{
+    public enum TweenTrigger
+    {
+        None,
+        Appear,
+        Disappear,
+        Trigger,
+        Click
+    }
+
+    public class GalaxyTweenController : MonoBehaviour
+    {
+        public List<GalaxyDOTweenAnimation> m_animationList = new List<GalaxyDOTweenAnimation>();
+        public Transform m_Target;
+
+        private void Awake()
+        {
+            m_animationList = new List<GalaxyDOTweenAnimation>(m_Target.GetComponentsInChildren<GalaxyDOTweenAnimation>(true));
+        }
+    }
+
+}
