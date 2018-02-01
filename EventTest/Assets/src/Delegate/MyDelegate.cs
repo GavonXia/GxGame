63e8273ddfaaaa6f1c4bfc736d3919c8a6c5d92f
Gavon.X <578417235@qq.com>
2018-02-01 10:01:29 +0800

    UnityEvent & Delegate

---
+++ EventTest/Assets/src/Delegate/MyDelegate.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void NoParamDelegate();
+
+public class MyDelegate
+{
+    public event NoParamDelegate MyEvent;
+
+}
---
+++ EventTest/Assets/src/UnityEvent/UnityEventListener.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityEventListener : MonoBehaviour
+{
+    MyUnityEvent uEvent = new MyUnityEvent();
+
+    // Update is called once per frame
+    void Update()
+    {
+        uEvent.AddListener(Test);
+
+    }
+
+    void Test()
+    {
+
+    }
+}
